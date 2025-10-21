35c7fd34e34ddf9ea4261542e029a1a34e4a24ad
Kevin420 <155149448+aliyosakevin428@users.noreply.github.com>
2025-10-21 21:14:38 +0800

    update news-section

--- resources/js/pages/welcome/partials/news-section.tsx
+++ resources/js/pages/welcome/partials/news-section.tsx
@@ -13,7 +13,7 @@ const NewsSection = () => {
             <div className="mx-auto w-full space-y-1 py-4 text-center">
                 <h3 className="px-4 text-3xl font-bold text-foreground">Kegiatan Gereja yang sedang berlangsung dan Warta Jemaat</h3>
                 <p className="px-4 py-5 text-lg text-foreground">Artikel kegiatan Gereja terbaru</p>
-                <div className="grid gap-6 md:grid-cols-4">
+                <div className="grid gap-6 md:grid-cols-3">
                     {news.map((berita) => (
                         <NewsItemCard news={berita} key={berita.id.toString()} />
                     ))}
